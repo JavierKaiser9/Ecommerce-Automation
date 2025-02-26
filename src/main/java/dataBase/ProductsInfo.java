93db45e00490270e968c506289e80ea425186b7f
JavierViteri <viteribasantes@gmail.com>
2025-02-26 16:35:40 +0100

    Third Draft

--- src/main/java/dataBase/ProductsInfo.java
+++ src/main/java/dataBase/ProductsInfo.java
@@ -2,6 +2,10 @@ package dataBase;
 
 public class ProductsInfo extends WebsiteData{
 
+    /**
+     * All Products in the website.
+     */
+
     public static String[] originalProductsNames = {"Sauce Labs Fleece Jacket", "Sauce Labs Backpack",
             "Sauce Labs Bolt T-Shirt","Sauce Labs Bike Light",
             "Sauce Labs Onesie", "Test.allTheThings() T-Shirt (Red)"};
